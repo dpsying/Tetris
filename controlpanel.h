1097bca5b20a170ba962a6b442d06592cd5cb118
dpsying <787701109@qq.com>
2016-09-13 18:00:36 +0800

    1

---
+++ controlpanel.h
@@ -0,0 +1,33 @@
+#ifndef CONTROLPANEL_H
+#define CONTROLPANEL_H
+
+#include <QWidget>
+#include "item.h"
+#include "qlabel.h"
+
+
+class ControlPanel : public QWidget
+{
+	Q_OBJECT
+
+public:
+	ControlPanel(QWidget *parent);
+	~ControlPanel();
+
+	void SetNext(ITEM_TYPE t,int nShape);
+	void SetScore(int n);
+	void SetSpeed(int n);
+protected:
+	void paintEvent(QPaintEvent *);
+
+
+private:
+	Item m_NextItem;
+
+	QLabel* m_pScore;
+
+	QLabel* m_pSpeed;
+	int m_nSpeed;
+};
+
+#endif // CONTROLPANEL_H
